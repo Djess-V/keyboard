734dbfb08d5e957d7f558ab5f5db69b80bcce89a
Djess-V <e.ageevets@yandex.ru>
2022-11-13 02:58:49 +0700

    Added pictures to notes

--- src/components/other/Note.jsx
+++ src/components/other/Note.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { CiWheat } from "react-icons/ci";
+import flowers from "../../assets/flowers.png";
+import snowman from "../../assets/snowman.png";
+import stars from "../../assets/stars.png";
+import girls from "../../assets/girls-fly-peace-pigeons.png";
+
+const image = [flowers, snowman, stars, girls];
 
 export default function Note({ data, modificator }) {
   return (
     <div className={`note_${modificator}`}>
-      <span>
-        <CiWheat fill="#000000" style={{ width: "20px", height: "20px" }} />
-      </span>
       {data.map((item) => (
         <p key={item}>{item}</p>
       ))}
+      <img
+        className={`image_${modificator}`}
+        src={image[modificator - 1]}
+        alt=""
+      />
     </div>
   );
 }
